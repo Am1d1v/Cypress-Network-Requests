7decb40ef6f436a8b4b14885fdb927d7041b2ede
Am1d1v <dimadiman1adima@gmail.com>
2024-06-22 18:49:05 +0300

    Takeaways test suite

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 import { seed } from './prisma/seed-test';
 
 export default defineConfig({
+
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
@@ -15,4 +16,5 @@ export default defineConfig({
       })
     },
   },
+  
 });
--- cypress/e2e/takeaways.cy.js
+++ cypress/e2e/takeaways.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="Cypress" />
 
 describe('Takeaways', () => {
+
   it('should display a list of fetched takeaways', () => {
-    cy.visit('/')
+    cy.visit('/');
+
+    // Check that first 2 takeaway-items rendered
+    cy.get('[data-cy="takeaway-item"]').should('have.length.above', 2);
   });
+
 });
