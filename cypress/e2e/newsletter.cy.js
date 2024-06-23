bb79e10f613616ec28644b6dde6545efece24a96
Am1d1v <dimadiman1adima@gmail.com>
2024-06-23 15:30:05 +0300

    Request Interception

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -2,15 +2,24 @@
 
 describe('News Letter', () => {
 
+    // Reseed database before every test
+    beforeEach(() => {
+        cy.task('seedDatabase');
+    });
+
     it('should display a success message', () => {
+        // Intercept any http request 
+        cy.intercept('POST', '/newsletter*', {status: 201, }).as('subscribe');
         cy.visit('/');
 
         // Select email input field then insert dummy email data
-        cy.get('[data-cy="newsletter-email"]').click().type('testemail@gmail.com');
+        cy.get('[data-cy="newsletter-email"]').type('testemail@gmail.com');
 
         // Click on "Sign Up" button
         cy.get('[data-cy="newsletter-submit"]').click()     
         
+        cy.wait('@subscribe');
+
         // Signing Up successfully message appears
         cy.get('div > p').contains('Thanks for signing up!');
     });
