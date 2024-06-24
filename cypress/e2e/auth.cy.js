f5b1858606926cd3cab40e1f0e544b31d2f5ece6
Am1d1v <dimadiman1adima@gmail.com>
2024-06-24 13:02:27 +0300

    login test case

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -26,4 +26,19 @@ describe('Authentication', () => {
         cy.getCookie('__session').its('value').should('not.be.empty');
     });
 
+    it.only('should login', () => {
+        cy.visit('//login');
+
+        // Select email input field and type email
+        cy.get('[data-cy="auth-email"]').click();
+        cy.get('[data-cy="auth-email"]').type('test@example.com');
+
+        // Select password input field and type password
+        cy.get('[data-cy="auth-password"]').click();
+        cy.get('[data-cy="auth-password"]').type('testpassword');
+
+        // Select "Login" button and click it
+        cy.get('[data-cy="auth-submit"]').click();
+    });
+
 });
