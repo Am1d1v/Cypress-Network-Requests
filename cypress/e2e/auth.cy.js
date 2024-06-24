6163ed6cc9e178ffd86ba62f701c51c6008a0773
Am1d1v <dimadiman1adima@gmail.com>
2024-06-24 13:09:55 +0300

    Logout test case

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -26,8 +26,8 @@ describe('Authentication', () => {
         cy.getCookie('__session').its('value').should('not.be.empty');
     });
 
-    it.only('should login', () => {
-        cy.visit('//login');
+    it('should login', () => {
+        cy.visit('/login');
 
         // Select email input field and type email
         cy.get('[data-cy="auth-email"]').click();
@@ -39,6 +39,36 @@ describe('Authentication', () => {
 
         // Select "Login" button and click it
         cy.get('[data-cy="auth-submit"]').click();
+
+        // After successed login user redirected to /takeaways
+        cy.location('pathname').should('eq', '/takeaways');
+
+        cy.getCookie('__session').its('value').should('not.be.empty');
+    });
+
+    it.only('should logout', () => {
+        cy.visit('//login');
+
+        // Loggin in
+        // Select email input field and type email
+        cy.get('[data-cy="auth-email"]').click();
+        cy.get('[data-cy="auth-email"]').type('test@example.com');
+
+        // Select password input field and type password
+        cy.get('[data-cy="auth-password"]').click();
+        cy.get('[data-cy="auth-password"]').type('testpassword');
+
+        // Select "Login" button and click it
+        cy.get('[data-cy="auth-submit"]').click();
+
+        // Select log out button and click it
+        cy.contains('Logout').click();
+
+        // After successed login user redirected to homepage
+        cy.location('pathname').should('eq', '/');
+
+        // Cookie value is empty
+        cy.getCookie('__session').its('value').should('be.empty');
     });
 
 });
